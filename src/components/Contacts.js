77b58707100123d14193bfbcc15c523f0e740f44
Stephen Blair <stephenjohnblair@outlook.com>
2020-06-30 12:07:51 +0100

    Completed contacts and portfolio pages and amended styles

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { makeStyles, withStyles} from '@material-ui/core/styles';
 import { TextField, Typography, Button, Grid, Box} from '@material-ui/core';
-import { SendIcon} from '@material-ui/icons';
+import { ArrowForward } from '@material-ui/icons';
 import Navbar from './Navbar';
 
+
+const useStyles = makeStyles((theme)    =>({
+    form: {
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        position: "absolute",
+    },
+    button: {
+        marginTop: "1rem",
+        color: "#fff",
+        borderColor: "#fe53bb",
+        fontSize: "1.2rem",
+    },
+    fields: {
+        width: "75vw",
+    },
+
+}));
 const InputField = withStyles({
     root: {
         "& label.Mui-focused": {
@@ -13,26 +31,44 @@ const InputField = withStyles({
         "& label": {
             color: "#fff",
         },
+        "& .MuiOutlinedInput-root": {
+            "& fieldset" :{
+                borderColor: "#fe53bb",  
+            },
+        "&:hover fieldset": {
+            borderColor: "#fe53bb",
+        },
+        "&.Mui-focused fieldset": {
+            borderColor: "#fe53bb",
+        }
+        }
     },
 })(TextField);
 
 const Contacts = () => {
+    const classes = useStyles();
+
     return (
         <>
         <Navbar />
-        <Box component="div">
+        <Box component="div" style={{background: "#262626", height: "100vh"}}>
             <Grid container justify="center">
-                <Box component="form">
-                    <Typography variant="h5">
+                <Box component="form" className={classes.form}>
+                    <Typography variant="h5" style={{ color: "#fff", textAlign: "center", textTransform: "uppercase"}}>
                         hire or contact me
                     </Typography>
-                    <InputField fullWidth={true} label="Name" variant="outlined" />
+                    <InputField className ={classes.fields} fullWidth={true} label="Name" variant="outlined" inputProps={{ style: { color:  "white"}}} margin="dense" size="medium" /> <br />
+                    <InputField fullWidth={true} label="EMail" variant="outlined" inputProps={{ style: { color:  "#fff"}}}  margin="dense" size="medium" /> <br/>
+                    <InputField fullWidth={true} label="Company Name" variant="outlined" inputProps={{ style: { color:  "#fff"}}}   margin="dense" size="medium"/> <br/>
+                    <Button className={classes.button} variant="outlined" fullWidth={true} endIcon={<ArrowForward />}>
+                        contact me 
+                    </Button>
                 </Box>
             </Grid>
         </Box>
         </>
-    )
-}
+    );
+};
 
 export default Contacts;
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,6 +3,14 @@ import {makeStyles} from '@material-ui/core/styles';
 import Navbar from  './Navbar';
 import { Box, Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import reactLogo from '../images/reactLogo.png';
+import adviceApp from '../images/adviceApp.png';
+import covid from '../images/covid.png';
+import weather from '../images/weather.png';
+import poshpods from '../images/poshpods.png';
+import numbers from '../images/numbers.png';
+import party from '../images/party.png';
+import empire from '../images/empire.png';
+import movie from '../images/movie.png';
 
 
 const useStyles = makeStyles({
@@ -12,11 +20,15 @@ const useStyles = makeStyles({
 
     },
     cardContainer: {
-        maxWidth: 345,
-        margin: "3rem 5rem 0 auto",
+        minWidth: 400,
+        margin: "3rem 3rem 2rem 3rem",
         border: "1px solid #ea00d9",
         padding: "0.5rem"
-    }
+    },
+    cardImage: {
+        width: "100%",
+        height: "auto",
+    },
 });
 
 const Portfolio =() => {
@@ -29,10 +41,10 @@ const Portfolio =() => {
             <Grid item xs={12} sm={8} md={4}>
             <Card className={classes.cardContainer}>
                 <CardActionArea>
-                    <CardMedia component="img" alt="Project 1" height="200" image={reactLogo}/>
+                    <CardMedia className={classes.cardImage} component="img" alt="Project 1" height="200" image={covid}/>
                     <CardContent>
                         <Typography gutterBottom variant="h5">
-                            COVID-19 Data Project
+                            COVID-19 Data Tracker
                         </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
                          A live COVID-19 data tracker using React and Charts.js.
@@ -54,21 +66,21 @@ const Portfolio =() => {
             <Grid item xs={12} sm={8} md={4}>
             <Card className={classes.cardContainer}>
                 <CardActionArea>
-                    <CardMedia component="img" alt="Project 1" height="200" image={reactLogo}/>
+                    <CardMedia className={classes.cardImage} component="img" alt="Project 1"  image={adviceApp}/>
                     <CardContent>
                         <Typography gutterBottom variant="h5">
-                            Project 2 
+                            Advice App
                         </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, laudantium.
+                            Gain some wisdom from this advice app, using React and the Advice Slip Json API.
                         </Typography>
                     </CardContent>
                     </CardActionArea>
                     <CardActions>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://github.com/stephenjblair/advice-app" target="_blank">
                             GitHub
                         </Button>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://advice-app-pi.now.sh/">
                             Live Demo
                         </Button>
                     </CardActions>
@@ -79,21 +91,21 @@ const Portfolio =() => {
             <Grid item xs={12} sm={8} md={4}>
             <Card className={classes.cardContainer}>
                 <CardActionArea>
-                    <CardMedia component="img" alt="Project 1" height="200" image={reactLogo}/>
+                    <CardMedia component="img" alt="Project 1" image={weather}/>
                     <CardContent>
                         <Typography gutterBottom variant="h5">
-                            Project 3
+                            Weather App
                         </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, laudantium.
+                            Five day weather forecast with React, Axios and Manchester Code's weather API.
                         </Typography>
                     </CardContent>
                     </CardActionArea>
                     <CardActions>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://github.com/stephenjblair/new-weather-app">
                             GitHub
                         </Button>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://sharp-swanson-c91733.netlify.app/">
                             Live Demo
                         </Button>
                     </CardActions>
@@ -104,21 +116,21 @@ const Portfolio =() => {
             <Grid item xs={12} sm={8} md={4}>
             <Card className={classes.cardContainer}>
                 <CardActionArea>
-                    <CardMedia component="img" alt="Project 1" height="140" image={reactLogo}/>
+                    <CardMedia component="img" alt="Project 1" image={poshpods}/>
                     <CardContent>
                         <Typography gutterBottom variant="h5">
-                            Project 4
+                            Posh Pods 
                         </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, laudantium.
+                            Commerical project using React, Express, Node and MongoDB. (Still in development)
                         </Typography>
                     </CardContent>
                     </CardActionArea>
                     <CardActions>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://github.com/stephenjblair/posh-pods-website">
                             GitHub
                         </Button>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://posh-pods-website.vercel.app/">
                             Live Demo
                         </Button>
                     </CardActions>
@@ -129,21 +141,21 @@ const Portfolio =() => {
             <Grid item xs={12} sm={8} md={4}>
             <Card className={classes.cardContainer}>
                 <CardActionArea>
-                    <CardMedia component="img" alt="Project 1" height="140" image={reactLogo}/>
+                    <CardMedia component="img" alt="Project 1" image={numbers}/>
                     <CardContent>
                         <Typography gutterBottom variant="h5">
-                            Project 5
+                            Numbers Game
                         </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, laudantium.
+                            A simple numbers game for state management using React.
                         </Typography>
                     </CardContent>
                     </CardActionArea>
                     <CardActions>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://github.com/stephenjblair/numbers-game">
                             GitHub
                         </Button>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://numbers-game-zeta.now.sh/">
                             Live Demo
                         </Button>
                     </CardActions>
@@ -154,21 +166,21 @@ const Portfolio =() => {
             <Grid item xs={12} sm={8} md={4}>
             <Card className={classes.cardContainer}>
                 <CardActionArea>
-                    <CardMedia component="img" alt="Project 1" height="140" image={reactLogo}/>
+                    <CardMedia component="img" alt="Project 1"  image={party}/>
                     <CardContent>
                         <Typography gutterBottom variant="h5">
-                            Project 6 
+                            Party Safari
                         </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, laudantium.
+                            Plan your night out in any city! Using HTML, CSS, JS and Google Maps/Places API.
                         </Typography>
                     </CardContent>
                     </CardActionArea>
                     <CardActions>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://github.com/stephenjblair/ifed-project">
                             GitHub
                         </Button>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://ifed-project.now.sh/">
                             Live Demo
                         </Button>
                     </CardActions>
@@ -179,21 +191,21 @@ const Portfolio =() => {
             <Grid item xs={12} sm={8} md={4}>
             <Card className={classes.cardContainer}>
                 <CardActionArea>
-                    <CardMedia component="img" alt="Project 1" height="140" image={reactLogo}/>
+                    <CardMedia component="img" alt="Project 1"  image={empire}/>
                     <CardContent>
                         <Typography gutterBottom variant="h5">
-                            Project 7
+                            Empire of the Sun
                         </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, laudantium.
+                            My first website! Band fan page using HTML, CSS and JS.
                         </Typography>
                     </CardContent>
                     </CardActionArea>
                     <CardActions>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://github.com/stephenjblair/ucd-project">
                             GitHub
                         </Button>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://ucd-project.now.sh/">
                             Live Demo
                         </Button>
                     </CardActions>
@@ -204,21 +216,21 @@ const Portfolio =() => {
             <Grid item xs={12} sm={8} md={4}>
             <Card className={classes.cardContainer}>
                 <CardActionArea>
-                    <CardMedia component="img" alt="Project 1" height="140" image={reactLogo}/>
+                    <CardMedia component="img" alt="Project 1" image={movie}/>
                     <CardContent>
                         <Typography gutterBottom variant="h5">
-                            Project 8 
+                            React Movie Search
                         </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, laudantium.
+                            Search and retrieve movie imformation from The Movie DB API using this React App.
                         </Typography>
                     </CardContent>
                     </CardActionArea>
                     <CardActions>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://github.com/stephenjblair/react-movie-search">
                             GitHub
                         </Button>
-                        <Button size="small" color="primary">
+                        <Button size="small" color="primary" href="https://react-movie-search.now.sh/">
                             Live Demo
                         </Button>
                     </CardActions>
